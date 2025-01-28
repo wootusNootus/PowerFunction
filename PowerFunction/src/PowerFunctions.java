e87328f0424ef472511c70473da8167a3527147a
wootusNootus <migospodinoff@gmail.com>
2025-01-28 10:06:55 -0600

    Time Test Commit

--- PowerFunction/src/PowerFunctions.java
+++ PowerFunction/src/PowerFunctions.java
@@ -3,6 +3,18 @@ public class PowerFunctions {
         System.err.println("Naive Power Function Check: " + ((naivePower(2,3) == 8) ? "Correct" : "Incorrect"));
         System.err.println("Unoptimized DC Power Function Check: " + ((unoptimizedDCPower(2,3) == 8) ? "Correct" : "Incorrect"));
         System.err.println("Optimized DC Power Function Check: " + ((optimizedDCPower(2,3) == 8) ? "Correct" : "Incorrect"));
+
+        // Testing for large x
+        System.out.println("Testing large x value execution time:");
+        testRuntime(() -> naivePower(1_000_000, 10));
+        testRuntime(() -> unoptimizedDCPower(1_000_000, 10));
+        testRuntime(() -> optimizedDCPower(1_000_000, 10));
+
+        // Testing for large n
+        System.out.println("Testing large x value execution time:");
+        testRuntime(() -> naivePower(2, 10000));
+        testRuntime(() -> unoptimizedDCPower(2, 10000));
+        testRuntime(() -> optimizedDCPower(2, 10000));
     }
 
     public static int naivePower(int x, int n){
@@ -45,5 +57,20 @@ public class PowerFunctions {
         else{
             return x * temp * temp;
         }
+    }
+
+    // Used the following website for tutorial
+    // https://www.tutorialspoint.com/how-to-implement-the-runnable-interface-using-lambda-expression-in-java
+    public static void testRuntime(Runnable function) {
+        // Testing runtime for x
+        long startTime = System.nanoTime();
+
+        // Running the function
+        function.run();
+
+        long endTime = System.nanoTime();
+
+        double duration = (endTime - startTime)/1_000_000.0;
+        System.out.println("Execution time: "+ duration + "ms");
     }
 }
